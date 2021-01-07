3e0157ec2f576322290e4d228cfe7139fdb5cde1
unChae <darrun45@gmail.com>
2021-01-07 17:51:42 +0900

    [chae] create basic env

---
+++ src/views/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading">
+    <v-progress-circular
+      class="spinner"
+      :size="70"
+      :width="7"
+      color="grey"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  opacity: 0.5;
+}
+.spinner {
+  position:absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
